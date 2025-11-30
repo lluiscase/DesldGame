a10a8e36c1bc8ca06c2e80cd65ad86dafb217400
lluiscase <luishenriqsa@gmail.com>
2025-11-30 00:39:30 -0300

    refactor: game is now about recicle

--- defaults.py
+++ defaults.py
@@ -1,5 +1,23 @@
 import pygame
+from item_trash import generate_enemies
 WINDOW_HEIGTH = 720
 WINDOW_WIDTH = 1280
 screen = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGTH))
+enemies_list =[generate_enemies() for _ in range(5)]
+
+class BoxTrash():
+    def __init__(self,x,y):
+        self.size = 125
+        self.x = x
+        self.y = y
+        self.position = pygame.Rect(self.x,self.y,self.size,self.size)
+        self.image = pygame.image.load("./img/500x500 darc.png")
+        self.image = pygame.transform.scale(self.image,(self.size,self.size))
+    def draw_box(self):
+        screen.blit(self.image,(self.x,self.y))
+    
+#Posições dos lixos
+display_trash = [{"x":100,"y":50},{"x":1080,"y":600},{"x":100,"y":600},{"x":1080,"y":50}]
+trash_boxes = [BoxTrash(pos["x"],pos["y"]) for pos in display_trash ]
+
 #def timer():
--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 import pygame
-from player import Player
-import enemies
 import defaults
-
+import player
 pygame.init()
+
 clock = pygame.time.Clock()
 running =True
 dt = 0
-
-jogador = Player()
-
-enemies_list =[enemies.generate_enemies() for _ in range(5)]
-
+active_box =None
+score = 0
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        active_box = player.mouseData(event=event,active_box=active_box)
+
     defaults.screen.fill((0, 0, 12)) 
-    jogador.sprite_player()
-    for e in enemies_list:
-        e.move()
+
+    #Desenha os lixos
+    for draw_boxes in defaults.trash_boxes:
+        draw_boxes.draw_box()
+    
+    #Desenha os itens que serão jogados fora
+    for e in defaults.enemies_list:
         e.sprite_enemie()
-    keys = pygame.key.get_pressed()
-    jogador.mov_player(keys,dt)
+    
     pygame.display.flip()
     dt = clock.tick(60) / 100
 pygame.quit()
